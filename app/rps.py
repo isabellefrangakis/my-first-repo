946174e2769d99743938d37d7ede6ff5a09fd82a
isabellefrangakis <ifrangakis@me.com>
2025-11-05 11:28:33 -0500

    Creating files

--- app/rps.py
+++ app/rps.py
@@ -3,6 +3,28 @@ import random
 
 VALID_OPTIONS = ["rock", "paper", "scissors"]
 
+
+def determine_winner(u,c):
+    if u == "rock" and c == "rock":
+        return "TIE GAME"
+    elif u == "rock" and c == "paper":
+        return "COMPUTER WINS"
+    elif u == "rock" and c == "scissors":
+        return "USER WINS"
+    elif u == "paper" and c == "rock":
+        return "COMPUTER WINS" # OOPS
+    elif u == "paper" and c == "paper":
+        return "TIE GAME"
+    elif u == "paper" and c == "scissors":
+        return "USER WINS"
+    elif u == "scissors" and c == "rock":
+        return "COMPUTER WINS"
+    elif u == "scissors" and c == "paper":
+        return "USER WINS"
+    elif u == "scissors" and c == "scissors":
+        return "TIE GAME"
+
+
 # ASK USER FOR AN INPUT (R/P/S)
 
 user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
@@ -26,27 +48,6 @@ print("COMP:", computer_choice)
 # we will soon move this into a function anyway
 u = user_choice
 c = computer_choice
-
-
-def determine_winner(u,c):
-    if u == "rock" and c == "rock":
-        return "TIE GAME"
-    elif u == "rock" and c == "paper":
-        return "COMPUTER WINS"
-    elif u == "rock" and c == "scissors":
-        return "USER WINS"
-    elif u == "paper" and c == "rock":
-        return "COMPUTER WINS" # OOPS
-    elif u == "paper" and c == "paper":
-        return "TIE GAME"
-    elif u == "paper" and c == "scissors":
-        return "USER WINS"
-    elif u == "scissors" and c == "rock":
-        return "COMPUTER WINS"
-    elif u == "scissors" and c == "paper":
-        return "USER WINS"
-    elif u == "scissors" and c == "scissors":
-        return "TIE GAME"
 
 result = determine_winner(user_choice, computer_choice)
 print(result)
